ec7f7e7990c2a309b54d5d86f8c683748fc2b943
anngelicafernanda <devanngelc@gmail.com>
2023-01-23 17:45:41 -0300

    Iterando sobre las categorias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,11 @@
 
 import { Button, Form, Row, Col, } from "react-bootstrap"
+import useCategorias from "../hooks/useCategoria"
 
 const Formulario = () => {
+
+    const {categorias} = useCategorias()
+
   return (
     <Form>
       <Row>
@@ -30,8 +34,27 @@ const Formulario = () => {
             >
 
                 <option value="">--Selecciona Categoría--</option>
+            {
+                categorias.map(categoria => (
+                    <option
+                    key={categoria.strCategory}
+                    value={categoria.strCategory}
+                    >
+                        {categoria.strCategory}
+                    </option>
+                ))
+            }
             </Form.Select>
         </Form.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-end">
+        <Col md={3}>
+        <Button variant="danger"
+        className="text-uppercase w-100">
+            Buscar Bebidas
+        </Button>
+        
         </Col>
       </Row>
     </Form>
