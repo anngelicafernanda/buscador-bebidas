1e36bdca14bc7f6dd27e06cd5751e8362ca5b086
anngelicafernanda <devanngelc@gmail.com>
2023-01-23 18:11:49 -0300

    Creando el segundo Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Container } from "react-bootstrap"
 import Formulario from "./components/Formulario"
 import { CategoriasProvider } from "./context/CategoriaProvider"
+import { BebidasProvider } from "./context/BebidasProvider"
 
 function App() {
 
   return (
     <CategoriasProvider>
+      <BebidasProvider>
   <header className="py-5">
     <h1>Buscador de Bebidas</h1>
   </header>
@@ -14,6 +16,7 @@ function App() {
     
     />
   </Container>
+  </BebidasProvider>
   </CategoriasProvider>
   )
 }
