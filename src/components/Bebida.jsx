9e7f3aee33b3f3b60f7943ec4c70eb68f14c0006
anngelicafernanda <devanngelc@gmail.com>
2023-01-24 11:35:01 -0300

    Agregando el componente Card a Bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,12 +1,27 @@
-import useBebidas from "../hooks/useBebidas"
+import { Button, Card, Col } from "react-bootstrap";
+import useBebidas from "../hooks/useBebidas";
 
-const Bebida = ({bebida}) => {
-const { bebidas} = useBebidas()
+const Bebida = ({ bebida }) => {
+  const { bebidas } = useBebidas();
   return (
-    <div>
-      {bebida.strDrink}
-    </div>
-  )
-}
+    <Col md={6} lg={3}>
+      <Card className="mb-4">
+        <Card.Img
+          variant="top"
+          src={bebida.strDrinkThumb}
+          alt={`Imagen de ${bebida.strDrink}`}
+        />
+        <Card.Body>
+          <Card.Title>{bebida.strDrink}</Card.Title>
 
-export default Bebida
+          <Button
+          variant={"warning"}
+          className="w-100 text-uppercase mt-2"
+          >Ver Receta</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Bebida;
